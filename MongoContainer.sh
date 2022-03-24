f055dff91dc7a8050df1bb6ed21f1c8087a0b532
vernillen <dylan.verniers@gmail.com>
2022-03-24 15:37:19 +0100

    -d added

--- MongoContainer.sh
+++ MongoContainer.sh
@@ -1,3 +1,3 @@
 #Mongo
-docker run -p 8080:27017 --name mongoDocker mongo
+docker run -d -p 8080:27017 --name mongoDocker mongo
 docker exec -it mongoDocker mongosh
--- RedisContainer.sh
+++ RedisContainer.sh
@@ -1,3 +1,3 @@
 #Redis
-docker run -p 8081:6379 --name redisDocker redis
+docker run -d -p 8081:6379 --name redisDocker redis
 docker exec -it redisDocker redis-cli

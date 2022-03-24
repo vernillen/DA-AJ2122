249d70055e78d30b1279fa4099d97975de1bb2e2
vernillen <dylan.verniers@gmail.com>
2022-03-24 18:44:32 +0100

    added packages to install

--- packages.bat
+++ packages.bat
@@ -3,3 +3,24 @@ python -m pip install redis
 
 :: Install mongo
 python -m pip install pymongo
+
+:: Install requests
+python -m pip install requests
+
+:: BeautifulSoup
+python -m pip install bs4
+
+:: Pandas
+python -m pip install pandas
+
+:: Json
+python -m pip install json
+
+:: ast
+python -m pip install ast
+
+:: urllib.parse
+python -m pip install urllib.parse
+
+:: time
+python -m pip install time
